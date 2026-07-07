d7e99d29d2b88a307c887fdb5401324c3a6d140b
madelainehidalgoclearminds-cell <madelainehidalgo.clearminds@gmail.com>
2026-07-07 07:59:05 -0500

    gato y comida

--- cazando.js
+++ cazando.js
@@ -1,6 +1,23 @@
 let canvas=document.getElementById("areaJuego")
 let cxt=canvas.getContext("2d")
-function dibujarPersonaje(){
+let gatoX =0
+let gatoY= 0
+let comidaX=0
+let comidaY= 0
+const ALTO_GATO=60
+const ANCHO_GATO=30
+const ALTO_COMIDA=23
+const ANCHO_COMIDA=23
+
+function dibujarIniciar(){
+    graficaComida()
+    graficaGato()
+}
+function graficaGato(){
     cxt.fillStyle="red"
-    cxt.fillRect(canvas.width/2,canvas.height-60,30,60)
+    cxt.fillRect(canvas.width/2,canvas.height-ALTO_GATO,ANCHO_GATO,ALTO_GATO)
+}
+function graficaComida(){
+    cxt.fillStyle="brown"
+    cxt.fillRect(0,0,ALTO_COMIDA,ANCHO_COMIDA)
 }
