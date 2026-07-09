bb2e990fa4c37ad5716f766e320eb4c40c8f43d5
madelainehidalgoclearminds-cell <madelainehidalgo.clearminds@gmail.com>
2026-07-09 08:36:39 -0500

    mover izquierda

--- cazando.js
+++ cazando.js
@@ -1,20 +1,20 @@
 let canvas=document.getElementById("areaJuego")
 let cxt=canvas.getContext("2d")
 let gatoX =0
-let gatoY= 0
+
 let comidaX=0
 let comidaY= 0
 const ALTO_GATO=60
 const ANCHO_GATO=30
 const ALTO_COMIDA=23
 const ANCHO_COMIDA=23
-
+let gatoY= canvas.height-ALTO_GATO
 function dibujarIniciar(){
     graficaComida()
     graficaGato()
 }
 function graficaGato(){
-    graficarRectangulo(canvas.width/2,canvas.height-ALTO_GATO,ANCHO_GATO,ALTO_GATO,"orange")
+    graficarRectangulo(gatoX,gatoY,ANCHO_GATO,ALTO_GATO,"orange")
     
 }
 function graficaComida(){
@@ -23,4 +23,13 @@ function graficaComida(){
 function graficarRectangulo(x,y,ancho,alto,color){
     cxt.fillStyle=color
     cxt.fillRect(x,y,ancho,alto)    
+}
+function limpiarCanvas(){
+    cxt.clearRect(0,0,canvas.width,canvas.height)
+}
+function moverIzquierda(){
+    gatoX=gatoX-10
+    limpiarCanvas()
+    graficaGato()
+    graficaComida()
 }
